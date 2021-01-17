7dfeab21885f8d45366675d7ea191e9d581a8e2b
Michael Sutanto <cea.smurf@gmail.com>
2021-01-17 19:46:07 +0800

    Develops into V2

--- js/main.js
+++ js/main.js
@@ -118,18 +118,17 @@
 	/*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
+//    $('.item-wrap a').magnificPopup({
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
+//       type:'inline',
+//       fixedContentPos: false,
+//       removalDelay: 300,
+//       showCloseBtn: false,
+//       mainClass: 'mfp-fade'
 
-   });
+//    });
 
    $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	e.preventDefault();
    	$.magnificPopup.close();
    });
 
